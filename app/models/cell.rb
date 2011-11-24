d898a6923c8aa2f7ce5924c24a7548d87dd8d938
yaqi zhao <rainy_star_2001@hotmail.com>
2011-11-24 03:33:17 -0600

    try saving data

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -1,35 +1,15 @@
 class Cell < ActiveRecord::Base
-  #def initialize(values)
-  #  raise Invalid, "Grid is the wrong size"+values.size().to_s unless values.length == 9
 
-  #  @values = values
-  #  @current_values = @values
-  #end
-  
   belongs_to :puzzle
-  
- # def values
- #   @values
- # end
-  
- # def values=(new_values)
- #   @values = new_values
- # end
   
   def [] (index)
     cell_value.split(//)[index.to_i]
   end
   
- # def current_vales
- #   @current_values
- # end
-  
- # def []= (index,new_value)
- #   a = @current_values.split(//)
- #   a[index] = new_value.to_s
- #   @current_values = a.join()
- # end
-  
+  def []= (index, value)
+    cell_value[index.to_i] = value.to_s
+  end
+
   def include? (value)
     cell_value.include? value.to_s
   end
